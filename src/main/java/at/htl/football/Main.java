fa742ccd929770f434a265f56eeb46fe607e5894
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2019-09-13 10:50:52 +0200

    Stand: Ende der Doppelstunde

--- src/main/java/at/htl/football/Main.java
+++ src/main/java/at/htl/football/Main.java
@@ -1,10 +1,24 @@
 package at.htl.football;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    Match match;
+    Team team;
+    League league;
 
     public static void main(String[] args) {
 
     }
+
+    public void readFile(String filename){
+        Path file = Paths.get("myfile.txt");
+       // Files.readAllLines();
+    }
+
 }
--- src/main/java/at/htl/football/Team.java
+++ src/main/java/at/htl/football/Team.java
@@ -1,4 +1,57 @@
 package at.htl.football;
 
 public class Team {
+    String name;
+    int points;
+    int wins;
+    int draws;
+    int defeats;
+    int goalsShot;
+    int goalsReceived;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public void addMatch(Match match){
+        return;
+    }
+
+    //region Getter
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public int getDefeats() {
+        return defeats;
+    }
+
+    public int getGoalsShot() {
+        return goalsShot;
+    }
+
+    public int getGoalsReceived() {
+        return goalsReceived;
+    }
+
+    //endregion
+
+    public int getGoalDifference(){
+        return -1;
+    }
+    public int copareTo(Team team){
+        return -1;
+    }
 }
